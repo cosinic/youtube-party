44e367872220b6714bd711134c52251c1c7a3904
Daven Wu <dwu@alk.com>
2019-08-06 21:05:42 -0400

    Add initial Vagrant files

---
+++ setup.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+# Install packages
+apt-get update
+apt-get install -y python3 
+apt-get install -y python3-venv 
+apt-get install -y python3-pip
+apt-get install -y git
+
+# Upgrade pip
+pip3 install --upgrade pip
+
+# Set up SSH and Github access
+mkdir -p ~/.ssh
+chmod 700 ~/.ssh
+ssh-keyscan -H github.com >> ~/.ssh/known_hosts
+
+# Set up directory & flask
+if [ ! -d "youtube-party" ]
+then
+    echo "Creating dir youtube-party..."
+    mkdir youtube-party
+fi
+
+cd youtube-party
+python3 -m venv venv
+. venv/bin/activate
+pip3 install Flask
